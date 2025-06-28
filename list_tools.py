7698209ea027908ef32a9e06176a06ee14a1c3d6
Rohan Gharibgard <rohang2449@gmail.com>
2025-06-27 22:05:45 -0700

    Added median function.

--- list_tools.py
+++ list_tools.py
@@ -1,3 +1,15 @@
-print("hello_world")
+import math
 
+def median(input):
+  if len(input) == 0:
+    return 0
+  mid_index = math.floor(len(input) / 2)
 
+  if len(input) % 2 == 0:
+    return (input[mid_index - 1] + input[mid_index]) / 2
+  else:
+    return input[mid_index]
+
+my_list = [1, 2, 3, 9, 4, 10]
+
+print(median(my_list))
